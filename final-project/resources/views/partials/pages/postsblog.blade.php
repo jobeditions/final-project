968be97457ad1682076a4a0f2fc4e805d7df6c09
ashokdev <jobeditions@gmail.com>
2017-06-18 20:39:47 +0200

    18th June Single Posts

--- final-project/resources/views/partials/pages/postsblog.blade.php
+++ final-project/resources/views/partials/pages/postsblog.blade.php
@@ -1,25 +1,25 @@
 
 		<div class="grid box">
 			<div class="grid-header">
-				<h3><a href="">{{ $posting->title }}</a></h3>
+				<h3><a href="/articles/{{ $posting->id }}">{{ $posting->title }}</a></h3>
 				<ul>
 				<li><span>Post By <a href="#"></a> on {{ $posting->created_at->format('F d,Y')}} </span></li>
 				<li><a href="#">Catégorie:{{$posting->category->name}}</a></li>
 				</ul>
 			</div>
 			<div class="grid-img-content">
-				<a href="articles/{{ $posting->id }}"><img src="/{{$posting->featured}}" width="350px" height="250px"/></a>
+				<a href="/articles/{{ $posting->id }}"><img src="/{{$posting->featured}}" width="350px" height="250px"/></a>
 				
 				{!! $posting->excerpt !!}
 
 				<div class="clearfix"> </div>
 			</div>
 			<div class="comments">
-			<ul>
+			<!--<ul>
 				<li><a href="#"><img src="/images/images/views.png" title="view" /></a></li>
 				<li><a href="#"><img src="/images/images/likes.png" title="likes" /></a></li>
 				<li><a href="#"><img src="/images/images/link.png" title="link" /></a></li>
 				
-			</ul>
+			</ul>-->
 			</div>
 		</div>
